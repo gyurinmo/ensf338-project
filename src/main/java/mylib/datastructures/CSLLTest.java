b61a4492c4a5f1ec8aebdd8f44311331fa0236c5
mariyahmalik <123036027+mariyahmalik@users.noreply.github.com>
2023-04-14 07:59:16 -0600

    Update CSLLTest.java

--- src/main/java/mylib/datastructures/CSLLTest.java
+++ src/main/java/mylib/datastructures/CSLLTest.java
@@ -1,63 +1,36 @@
 package src.main.java.mylib.datastructures;
 
-import org.junit.Test;
 import src.main.java.mylib.datastructures.linear.CSLL;
 import src.main.java.mylib.datastructures.nodes.SNode;
 
-import static org.junit.Assert.*;
-
 public class CSLLTest {
-
-    @Test
-    public void testInsertTail() {
+    public static void main(String[] args) {
         CSLL<Integer> csll = new CSLL<>();
+
+        System.out.println("Inserting values 1, 2, and 3 at tail");
         csll.insertTail(new SNode<>(1));
         csll.insertTail(new SNode<>(2));
         csll.insertTail(new SNode<>(3));
-        assertEquals(3, csll.getSize());
-        assertEquals(Integer.valueOf(1), csll.getHead().getData());
-        assertEquals(Integer.valueOf(3), csll.getHead().getData());
+        System.out.println("Expected output: 1 2 3");
+        System.out.println("Actual output: ");
+        csll.print();
 
-    }
-
-    @Test
-    public void testDeleteHead() {
-        CSLL<Integer> csll = new CSLL<>();
-        csll.insertTail(new SNode<>(1));
-        csll.insertTail(new SNode<>(2));
-        csll.insertTail(new SNode<>(3));
+        System.out.println("Deleting head");
         csll.deleteHead();
-        assertEquals(2, csll.getSize());
-        assertEquals(Integer.valueOf(2), csll.getHead().getData());
-        assertEquals(Integer.valueOf(3), csll.getHead().getData());
-    }
+        System.out.println("Expected output: 2 3");
+        System.out.println("Actual output: ");
+        csll.print();
 
-    @Test
-    public void testDeleteTail() {
-        CSLL<Integer> csll = new CSLL<>();
-        csll.insertTail(new SNode<>(1));
-        csll.insertTail(new SNode<>(2));
-        csll.insertTail(new SNode<>(3));
+        System.out.println("Deleting tail");
         csll.deleteTail();
-        assertEquals(2, csll.getSize());
-        assertEquals(Integer.valueOf(1), csll.getHead().getData());
-        assertEquals(Integer.valueOf(2), csll.getHead().getData());
+        System.out.println("Expected output: 2");
+        System.out.println("Actual output: ");
+        csll.print();
 
-    }
-
-    @Test
-    public void testDelete() {
-        CSLL<Integer> csll = new CSLL<>();
-        SNode<Integer> node1 = new SNode<>(1);
-        SNode<Integer> node2 = new SNode<>(2);
-        SNode<Integer> node3 = new SNode<>(3);
-        csll.insertTail(node1);
-        csll.insertTail(node2);
-        csll.insertTail(node3);
-        csll.delete(node2);
-        assertEquals(2, csll.getSize());
-        assertEquals(node1, csll.getHead());
-        assertEquals(node3, csll.getTail());
-
+        System.out.println("Deleting value 2");
+        csll.delete(new SNode<>(2));
+        System.out.println("Expected output: ");
+        System.out.println("Actual output: ");
+        csll.print();
     }
 }
