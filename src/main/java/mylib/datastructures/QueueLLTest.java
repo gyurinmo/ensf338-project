7ec9827b314fa9d67436bb6e86f5e598e3e818a8
mariyahmalik <123036027+mariyahmalik@users.noreply.github.com>
2023-04-14 08:00:34 -0600

    Update QueueLLTest.java

--- src/main/java/mylib/datastructures/QueueLLTest.java
+++ src/main/java/mylib/datastructures/QueueLLTest.java
@@ -1,63 +1,29 @@
 package src.main.java.mylib.datastructures;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-import java.beans.Transient;
-
 import src.main.java.mylib.datastructures.linear.QueueLL;
 import src.main.java.mylib.datastructures.nodes.SNode;
 
 public class QueueLLTest {
+    public static void main(String[] args) {
+        QueueLL<Integer> queue = new QueueLL<>();
 
-    @Test
-    public void testEnqueueAndDequeue() {
-        QueueLL<String> queue = new QueueLL<>();
+        queue.enqueue(new SNode<>(1));
+        queue.enqueue(new SNode<>(2));
+        queue.enqueue(new SNode<>(3));
+        queue.enqueue(new SNode<>(4));
 
-        queue.enqueue(new SNode<>("A"));
-        queue.enqueue(new SNode<>("B"));
-        queue.enqueue(new SNode<>("C"));
+        System.out.println("Queue: ");
+        queue.print();
 
-        assertEquals("A", queue.getHead().getData());
-        assertEquals("B", queue.getHead().getNext().getData());
-        assertEquals("C", queue.getTail().getData());
+        System.out.println("Dequeue: " + queue.dequeue().getData());
+        System.out.println("Dequeue: " + queue.dequeue().getData());
 
-        SNode<String> dequeuedNode = queue.dequeue();
-        assertEquals("A", dequeuedNode.getData());
+        System.out.println("Queue: ");
+        queue.print();
 
-        assertEquals("B", queue.getHead().getData());
+        queue.enqueue(new SNode<>(5));
 
-        dequeuedNode = queue.dequeue();
-        assertEquals("B", dequeuedNode.getData());
-        dequeuedNode = queue.dequeue();
-        assertEquals("C", dequeuedNode.getData());
-
-        assertNull(queue.getHead());
-        assertNull(queue.getTail());
-
-    }
-
-    @Test
-    public void testInsertHead() {
-        QueueLL<String> queue = new QueueLL<>();
-
-        try {
-            queue.insertHead(new SNode<>("A"));
-            fail("Expected UnsupportedOperationException");
-        } catch (UnsupportedOperationException e) {
-
-        }
-    }
-
-    @Test
-    public void testSortedInsert() {
-        QueueLL<String> queue = new QueueLL<>();
-
-        try {
-            queue.sortedInsert(new SNode<>("A"));
-            fail("Expected UnsupportedOperationException");
-        } catch (UnsupportedOperationException e) {
-
-        }
+        System.out.print("Queue: ");
+        queue.print();
     }
 }
