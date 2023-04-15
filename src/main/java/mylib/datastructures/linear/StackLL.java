ddb2c2fc5cc3ebf8fc63c0884af4165bc9dc2151
Jane <gyurin.mo@ucalgary.ca>
2023-04-15 13:33:15 -0600

    new StackLL class and StackLLTest code

--- src/main/java/mylib/datastructures/linear/StackLL.java
+++ src/main/java/mylib/datastructures/linear/StackLL.java
@@ -1,53 +1,109 @@
 package src.main.java.mylib.datastructures.linear;
 
 import src.main.java.mylib.datastructures.nodes.SNode;
-import java.util.EmptyStackException;
 
-public class StackLL<T> extends SLL<T> {
+public class StackLL extends SLL {
+    private SNode head;
+    private int size;
+    private boolean sorted;
+    
+    public StackLL() {
+        super();
+        this.head = null;
+        this.size = 0;
+        this.sorted = true;
+    }
+
+    public StackLL(SNode head) {
+        super(head);
+        this.head = head;
+        this.size = 1;
+        this.sorted = true;
+    }
+
+    public int peek() {
+        return this.head.getData();
+    }
+
+    public void push(SNode node) {
+        super.insertHead(node);
+        this.head = node;
+        size++;
+        this.sorted = false;
+    }
+
+    public void pop() {
+        super.deleteHead();
+        head = head.getNext();
+        size--;
+    }
+
+    public void sortedPush(SNode node) {
+        if (head == null) {
+            head = node;
+        } else {
+            if (!sorted) {
+                sort();
+            }
+            if (node.getData() <= head.getData()) {
+                node.setNext(head);
+                head = node;
+            } else {
+                SNode current = head;
+                while (current.getNext() != null && node.getData() > current.getNext().getData()) {
+                    current = current.getNext();
+                }
+                node.setNext(current.getNext());
+                current.setNext(node);
+            }
+            size++;
+            this.sorted = true;
+        }
+    }
 
     @Override
-    public void insertTail(SNode<T> node) {
-
+    public SNode search(SNode node) {
+        SNode result = super.search(node);
+        return result;
     }
-
-    @Override 
-    public void insert(SNode<T> node, int position) {
-
-    }
-
-    @Override
-    public void sortedInsert(SNode<T> node) {
-
-    }
-
-    @Override
-    public void delete(SNode<T> node) {
-
-    }
-
+    
     @Override
     public void sort() {
-
+        super.sort();
+        this.sorted = true;
     }
 
-    public void push(T data) {
-        insertHead(new SNode<>(data));
+    @Override
+    public void delete(SNode node) {
+        super.delete(node);
     }
-
-    public T pop() {
-        SNode<T> top = getHead();
-        if (top == null) {
-            throw new EmptyStackException();
+    
+    @Override
+    public void clear() {
+        super.clear();
+        this.head = null;
+        this.size = 0;
+        this.sorted = true;
+    }
+    
+    @Override
+    public void print() {
+        System.out.println("Stack size: " + size);
+        System.out.println("Sorted status: " + (sorted ? "sorted" : "not sorted"));        
+        System.out.print("Stack content (top to bottom): ");
+        SNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.getData() + " ");
+            curr = curr.getNext();
         }
-        deleteHead();
-        return top.getData();
+        System.out.println();
+    }  
+
+    @Override
+    public void insertTail(SNode node) {
     }
 
-    public T peek() {
-        SNode<T> top = getHead();
-        if (top == null) {
-            throw new EmptyStackException();
-        }
-        return top.getData();
+    @Override
+    public void deleteTail() {
     }
 }
--- src/main/java/mylib/datastructures/test/StackLLTest.java
+++ src/main/java/mylib/datastructures/test/StackLLTest.java
@@ -1,29 +1,199 @@
 package src.main.java.mylib.datastructures.test;
 
-import java.util.EmptyStackException;
-
 import src.main.java.mylib.datastructures.linear.StackLL;
+import src.main.java.mylib.datastructures.nodes.SNode;
 
 public class StackLLTest {
     public static void main (String[] args) {
-        StackLL<Integer> stack = new StackLL<>();
 
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        System.out.println("Stack after push operations: " + stack.toString());
+        // Test StackLL Class methods 
+        System.out.println();
+        System.out.println();
+        System.out.println("***Testing StackLL Class...***");
+    
 
-        System.out.println("Peek the top element: " + stack.peek());
+        // create a new StackLL object by using overloaded constructor StackLL(SNode head)
+        SNode head = new SNode(5);
+        System.out.println();
+        System.out.println("SNode(int data) method successfully created a SNode object with data 5.");;       
+        StackLL stack1 = new StackLL(head);
+        
+        System.out.println("Overloaded constructor StackLL(SNode head) created a new StackLL object \"stack1\".");
+        System.out.println();
 
-        System.out.println("Pop the top element: " + stack.pop());
-        System.out.println("Stack after pop operation: " + stack.toString());
+        // expected result
+        System.out.println("Expected Result: ");
+        System.out.println("Stack size: 1");
+        System.out.println("Sorted Status: sorted");
+        System.out.println("Stack content (top to bottom): 5");
+        System.out.println();
 
-        try {
-            stack.pop();
+        // actual result displayed by print()
+        System.out.println("Actual Result: ");
+        stack1.print();
+        System.out.println();    
 
-        } catch (EmptyStackException e) {
-            System.out.println("Caught an EmptyStackException when trying to pop from an empty stack.");
+
+        // test inherited getHead() method from SLL class via peek() method
+        stack1.peek();
+        System.out.println();
+        System.out.println("peek() successfully returned head value of previosuly crated StackLL object \"stack1\".");
+        System.out.println();
+
+        // expected result
+        System.out.println("Expected head: 5");
+
+        // actual result displayed by print()
+        System.out.print("Actual Result: ");
+        System.out.println(stack1.peek());
+        System.out.println();
+
+
+        // create a new StackLL object by using default constructor StackLL()
+        StackLL stack2 = new StackLL();
+        System.out.println();
+        System.out.println("Default constructor StackLL() successfully created a new StackLL object \"stack2\".");
+        System.out.println();
+    
+        // test inherited insertHead(SNode node) method from SLL class via push(SNode node) method
+        stack2.push(new SNode(3));
+        System.out.println("push(SNode node) method successfully inserted 9 to the top of \"stack2\".");
+
+        stack2.push(new SNode(1));
+        System.out.println("push(SNode node) method successfully inserted 3 to the top of \"stack2\".");
+
+        stack2.push(new SNode(9));
+        System.out.println("push(SNode node) method successfully inserted 1 to the top of \"stack2\".");
+
+        // expected result
+        System.out.println();
+        System.out.println("Expected Result: ");
+        System.out.println("Stack size: 3");
+        System.out.println("Sorted Status: not sorted");
+        System.out.println("Stack content (top to bottom): 9 1 3");
+        System.out.println();
+
+        // actual result displayed by test()
+        System.out.println("Actual Result: ");
+        stack2.print();
+        System.out.println();
+
+
+        // test inherited sort() method from SLL class
+        stack2.sort();
+        System.out.println();
+        System.out.println("sort() method successfully sorted contents in \"stack2\" in an ascending order.");
+        System.out.println();
+
+        // expected result
+        System.out.println("Expected Result: ");
+        System.out.println("Stack size: 3");
+        System.out.println("Sorted Status: sorted");
+        System.out.println("Stack content (top to bottom): 1 3 9");
+        System.out.println();
+
+        // actual result displayed by test()
+        System.out.println("Actual Result: ");
+        stack2.print();
+        System.out.println();
+
+        // test sortedPush(SNode node)
+        SNode node1 = new SNode(7);
+        System.out.println();
+        System.out.println("SNode(int data) method successfully created a SNode object with data 7.");;
+        stack2.sortedPush(node1);
+        System.out.println("sortedPush(SNode node) method successfully inserted a new SNode with \"data\" 7 in a sorted order.");
+        System.out.println();
+
+        // expected result
+        System.out.println("Expected Result: ");
+        System.out.println("Stack size: 4");
+        System.out.println("Sorted Status: sorted");
+        System.out.println("Stack content (top to bottom): 1 3 7 9");
+        System.out.println();
+
+        // actual result displayed by test()
+        System.out.println("Actual Result: ");
+        stack2.print();
+        System.out.println();
+
+
+        // test inherited search(SNode node) from SLL calss, successful case
+        System.out.println();
+        System.out.println("Testing search(SNode node) method by using previously created StackLL object \"stack2\"...");
+        System.out.println();
+
+        SNode nodeToSearch1 = new SNode(3);
+        System.out.println("SNode(int data) method successfully created a SNode object with data 3.");;
+
+        // expected result
+        System.out.print("Expected Search Result: ");
+        System.out.println("Found node with value 3");
+    
+        // actual result
+        System.out.print("Actual Search Result: ");
+        SNode foundNode1 = stack2.search(nodeToSearch1);
+        if (foundNode1 != null) {
+            System.out.println("Found node with value " + foundNode1.getData());
+        } else {
+            System.out.println("Node not found");
         }
+
+        // test inherited search(SNode node) from SLL calss, not successful case
+        SNode nodeToSearch2 = new SNode(6);
+        System.out.println();
+        System.out.println("SNode(int data) method successfully created a Node object with data 6.");;
+
+        // expected result
+        System.out.print("Expected Search Result: ");
+        System.out.println("Node not found");
+    
+        // actual result
+        System.out.print("Actual Search Result: ");
+        SNode foundNode2 = stack2.search(nodeToSearch2);
+        if (foundNode2 != null) {
+            System.out.println("Found node with value " + foundNode2.getData());
+        } else {
+            System.out.println("Node not found");
+        }
+        System.out.println();
+
+
+        // test inherited deleteHead() method from SLL via pop() method
+        stack2.pop();
+        System.out.println();
+        System.out.println("pop() method successfully deleted the head of \"stack2\".");
+        System.out.println();
+
+        // expected result
+        System.out.println("Expected Result: ");
+        System.out.println("List Length: 3");
+        System.out.println("Sorted Status: sorted");
+        System.out.println("List Content: 3 7 9");
+        System.out.println();
+
+        // actual result displayed by print()
+        System.out.println("Actual Result: ");
+        stack2.print();
+        System.out.println();
+
+
+        // test inherited clear() method from SLL class
+        stack2.clear();
+        System.out.println("clear() method successfully removed all contents in the previously created StackLL object \"stack2\".");
+        System.out.println();
+
+        System.out.println("Expected Result: ");
+        System.out.println("Stack size: 0");
+        System.out.println("Sorted Status: sorted");
+        System.out.println("Stack Content: ");
+        System.out.println();
+
+        System.out.println("Actual Result: ");
+        stack2.print();
+
+        // end of  the test
+        System.out.println();
+        System.out.println("***StackLL Class Testing Completed!***");
     }
 }
-
