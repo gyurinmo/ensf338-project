577cdaa2405e706db180a047189f019f8f543954
Jane <gyurin.mo@ucalgary.ca>
2023-04-15 15:29:41 -0600

    New QueueLL class and QueueLLTest code

--- src/main/java/mylib/datastructures/linear/QueueLL.java
+++ src/main/java/mylib/datastructures/linear/QueueLL.java
@@ -2,28 +2,127 @@ package src.main.java.mylib.datastructures.linear;
 
 import src.main.java.mylib.datastructures.nodes.SNode;
 
-public class QueueLL<T> extends SLL<T> {
+public class QueueLL extends SLL {
+    private SNode head;
+    private SNode tail;
+    private int size;
+    
     public QueueLL() {
         super();
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
     }
 
-    public void enqueue(SNode<T> node) {
-        super.insertTail(node);
+    public QueueLL(SNode head) {
+        super(head);
+        this.head = head;
+        this.tail = head;
+        this.size = 1;
     }
 
-    public SNode<T> dequeue() {
-        SNode<T> node = super.getHead();
+    public int peek() {
+        return this.head.getData();
+    }
+
+    public void enqueue(SNode node) {
+        if (tail == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.setNext(node);
+            tail = node;
+        }
+        size++;
+    }
+
+    @Override
+    public SNode search(SNode node) {
+        SNode current = head;
+
+        while (current != null) {
+            if (current.getData() == node.getData()) {
+                return current;
+            }
+            current = current.getNext();
+        }
+        return null;
+    }
+    
+    public void dequeue() {
         super.deleteHead();
-        return node;
+        head = head.getNext();
+        size--;
+    }
+    
+    @Override
+    public void delete(SNode node) {
+        if (head == null) {  
+            System.out.println("The list is empty.");
+            System.out.println();
+            return;
+        }
+        if (node.equals(head)) {  
+            deleteHead();
+            return;
+        }
+        if (node.equals(tail)) {  
+            deleteTail();
+            return;
+        }
+        
+        SNode prev = head;
+        while (prev.getNext() != null && !prev.getNext().equals(node)) {
+            prev = prev.getNext();
+        }
+        
+        if (prev.getNext() == null) {  
+            return;
+        }
+        
+        prev.setNext(prev.getNext().getNext());
+        size--;
+    }  
+
+    @Override
+    public void clear() {
+        super.clear();
+        head = null;
+        tail = null;
+        size = 0;
     }
 
     @Override
-    public void insertHead(SNode<T> node) {
-
+    public void print() {
+        System.out.println("Queue length: " + size);
+        System.out.print("Queue content (front to rear): ");
+        SNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.getData() + " ");
+            curr = curr.getNext();
+        }
+        System.out.println();
+    }
+    
+    @Override
+    public void insert(SNode node, int position) {
     }
 
     @Override
-    public void sortedInsert(SNode<T> node) {
-
+    public void insertHead(SNode node) {
     }
+
+    @Override
+    public void sort() {
+    }
+
+    @Override
+    public void sortedInsert(SNode node) {
+    }
+
+    @Override
+    public void deleteTail() {
+    }
+
+    
 }
--- src/main/java/mylib/datastructures/test/QueueLLTest.java
+++ src/main/java/mylib/datastructures/test/QueueLLTest.java
@@ -5,25 +5,154 @@ import src.main.java.mylib.datastructures.nodes.SNode;
 
 public class QueueLLTest {
     public static void main(String[] args) {
-        QueueLL<Integer> queue = new QueueLL<>();
+        
+        // Test QueueLL Class methods 
+        System.out.println();
+        System.out.println();
+        System.out.println("***Testing QueueLL Class...***");
+    
 
-        queue.enqueue(new SNode<>(1));
-        queue.enqueue(new SNode<>(2));
-        queue.enqueue(new SNode<>(3));
-        queue.enqueue(new SNode<>(4));
+        // create a new QueueLL object by using overloaded constructor QueueLL(SNode head)
+        SNode head = new SNode(5);
+        System.out.println();
+        System.out.println("SNode(int data) method successfully created a SNode object with data 5.");;       
+        QueueLL queue1 = new QueueLL(head);
+        
+        System.out.println("Overloaded constructor QueueLL(SNode head) created a new QueueLL object \"queue1\".");
+        System.out.println();
 
-        System.out.println("Queue: ");
-        queue.print();
+        // expected result
+        System.out.println("Expected Result: ");
+        System.out.println("Queue size: 1");
+        System.out.println("Queuecontent (front to rear): 5");
+        System.out.println();
 
-        System.out.println("Dequeue: " + queue.dequeue().getData());
-        System.out.println("Dequeue: " + queue.dequeue().getData());
+        // actual result displayed by print()
+        System.out.println("Actual head: ");
+        queue1.print();
+        System.out.println();    
 
-        System.out.println("Queue: ");
-        queue.print();
 
-        queue.enqueue(new SNode<>(5));
+        // test peek() method
+        queue1.peek();
+        System.out.println();
+        System.out.println("peek() successfully returned head value of previosuly crated QueueLL object \"queue1\".");
+        System.out.println();
 
-        System.out.print("Queue: ");
-        queue.print();
+        // expected result
+        System.out.println("Expected head: 5");
+
+        // actual result displayed by print()
+        System.out.print("Actual Result: ");
+        System.out.println(queue1.peek());
+        System.out.println();
+
+
+        // create a new QueueLL object by using default constructor QueueLL()
+        QueueLL queue2 = new QueueLL();
+        System.out.println();
+        System.out.println("Default constructor QueueLL() successfully created a new QueueLL object \"queue2\".");
+        System.out.println();
+    
+        // test inherited insertTail(SNode node) method from SLL class via enqueue(SNode node) method
+        queue2.enqueue(new SNode(3));
+        System.out.println("enqueue(SNode node) method successfully inserted 3 to the bottom of \"queue2\".");
+
+        queue2.enqueue(new SNode(1));
+        System.out.println("enqueue(SNode node) method successfully inserted 1 to the bottom of \"queue2\".");
+
+        queue2.enqueue(new SNode(9));
+        System.out.println("enqueue(SNode node) method successfully inserted 9 to the bottom of \"queue2\".");
+       
+        // expected result
+        System.out.println();
+        System.out.println("Expected Result: ");
+        System.out.println("Queue size: 3");
+        System.out.println("Queue content (front to rear): 3 1 9");
+        System.out.println();
+
+        // actual result displayed by test()
+        System.out.println("Actual Result: ");
+        queue2.print();
+        System.out.println();
+
+        // test inherited search(SNode node) from SLL calss, successful case
+        System.out.println();
+        System.out.println("Testing search(SNode node) method by using previously created QueueLL object \"queue2\"...");
+        System.out.println();
+
+        SNode nodeToSearch1 = new SNode(3);
+        System.out.println("SNode(int data) method successfully created a SNode object with data 3.");;
+
+        // expected result
+        System.out.print("Expected Search Result: ");
+        System.out.println("Found node with value 3");
+    
+        // actual result
+        System.out.print("Actual Search Result: ");
+        SNode foundNode1 = queue2.search(nodeToSearch1);
+        if (foundNode1 != null) {
+            System.out.println("Found node with value " + foundNode1.getData());
+        } else {
+            System.out.println("Node not found");
+        }
+
+        // test inherited search(SNode node) from SLL calss, not successful case
+        SNode nodeToSearch2 = new SNode(6);
+        System.out.println();
+        System.out.println("SNode(int data) method successfully created a Node object with data 6.");;
+
+        // expected result
+        System.out.print("Expected Search Result: ");
+        System.out.println("Node not found");
+    
+        // actual result
+        System.out.print("Actual Search Result: ");
+        SNode foundNode2 = queue2.search(nodeToSearch2);
+        if (foundNode2 != null) {
+            System.out.println("Found node with value " + foundNode2.getData());
+        } else {
+            System.out.println("Node not found");
+        }
+        System.out.println();
+
+
+        // test inherited deleteHead() method from SLL via dequeue() method
+        queue2.dequeue();
+        System.out.println();
+        System.out.println("dequeue() method successfully deleted the head of \"queue2\".");
+        System.out.println();
+
+        // expected result
+        System.out.println("Expected Result: ");
+        System.out.println("Queue Length: 2");
+        System.out.println("Queue Content (front to rear): 1 9 ");
+        System.out.println();
+
+        // actual result displayed by print()
+        System.out.println("Actual Result: ");
+        queue2.print();
+        System.out.println();
+
+
+        // test inherited clear() method from SLL class
+        queue2.clear();
+        System.out.println();
+        System.out.println("clear() method successfully removed all contents in the previously created QueueLL object \"queue2\".");
+        System.out.println();
+
+        System.out.println("Expected Result: ");
+        System.out.println("Queue size: 0");
+        System.out.println("Queue Content (front to rear): ");
+        System.out.println();
+
+        System.out.println("Actual Result: ");
+        queue2.print();
+
+
+        // end of  the test
+        System.out.println();
+        System.out.println("***QueueLL Class Testing Completed!***");
     }
+
 }
