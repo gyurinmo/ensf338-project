c520a7bfbe812cd44657f1c9868a720f33bbe6b0
Jane <gyurin.mo@ucalgary.ca>
2023-04-15 04:31:00 -0600

    New CSLL class and CSLLTest file

--- src/main/java/mylib/datastructures/linear/CSLL.java
+++ src/main/java/mylib/datastructures/linear/CSLL.java
@@ -2,87 +2,273 @@ package src.main.java.mylib.datastructures.linear;
 
 import src.main.java.mylib.datastructures.nodes.SNode;
 
-public class CSLL<T> extends SLL<T> {
-    private SNode<T> head;
-    private SNode<T> tail;
+public class CSLL extends SLL {
+    private SNode head;
+    private SNode tail;
     private int size;
+    private boolean sorted;
 
     public CSLL() {
-        super();
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+        this.sorted = true;
+
+        if (head != null) {
+            tail.setNext(head);
+        }
     }
 
-    public CSLL(SNode<T> head) {
-        super(head);
-        head.setNext(head);
+    public CSLL(SNode node) {
+        this.head = node;
+        this.tail = node;
+        this.sorted = true;
+
+        if (head != null) {
+            tail = head;
+            while (tail.getNext() != null) {
+                tail = tail.getNext();
+            }
+            tail.setNext(head);
+            size++;
+        } else {
+            tail = null;
+        }
     }
 
+    public SNode getHead() {return this.head;}
+    public SNode getTail() {return this.tail;}
+    public int getSize() {return this.size;}
+    
     @Override
-    public void insertTail(SNode<T> node) {
-        if (tail == null) {
+    public void insertHead(SNode node) {
+        if (head == null) {
             head = node;
             tail = node;
             node.setNext(head);
         } else {
             node.setNext(head);
-            tail.setNext(node);
-            tail = node;
+            head = node;
+            tail.setNext(head);
         }
         size++;
     }
 
     @Override
-    public void deleteHead() {
-        if (head != null) {
-            head = head.getNext();
-            tail.setNext(head);
-            size--;
-            if (head == null) {
-                tail = null;
-            }
+    public void insertTail(SNode node) {
+        if (tail == null) {
+            head = node;
+            tail = node;
+            node.setNext(head);
+        } else {
+            tail.setNext(node);
+            tail = node;
+            node.setNext(head);
         }
+        size++;
     }
 
     @Override
-    public void deleteTail() {
-        if (size == 0) {
+    public void insert(SNode node, int position) {
+        if (position < 0 || position > size) {
+            throw new IndexOutOfBoundsException();
+        } else if (position == 0) {
+            if (size == 0) { 
+                node.setNext(node); 
+                head = node;
+                tail = node;
+            } else {
+                node.setNext(head);
+                head = node;
+                tail.setNext(head); 
+            }
+            size++;
+        } else if (position == size) {
+            insertTail(node);
+        } else {
+            SNode current = head;
+            for (int i = 0; i < position - 1; i++) {
+                current = current.getNext();
+            }
+            node.setNext(current.getNext());
+            current.setNext(node);
+            size++;
+        }
+        this.sorted = false;
+    }
+
+    @Override
+    public void sortedInsert(SNode node) {
+        if (head == null) {
+            this.head = node;
+            this.tail = node;
+            node.setNext(node);
+            size++;
+            this.sorted = true;
             return;
         }
-        if (size == 1) {
+        if (!sorted) {
+            sort();
+        }
+        
+        SNode prev = null;
+        SNode curr = head;
+        do {
+            if (node.getData() <= curr.getData()) {
+                break;
+            }
+            prev = curr;
+            curr = curr.getNext();
+        } while (curr != head);
+    
+        if (prev == null) {
+            node.setNext(head);
+            head = node;
+            tail.setNext(node);
+        } else {
+            prev.setNext(node);
+            node.setNext(curr);
+            if (curr == head) {
+                tail = node;
+            }
+        }
+        size++;
+        this.sorted = true;
+    }
+    
+    @Override
+    public SNode search(SNode node) {
+        if (head == null) {
+            return null;
+        }
+        
+        SNode current = head;
+        
+        do {
+            if (current.getData() == node.getData()) {
+                return current;
+            }
+            current = current.getNext();
+        } while (current != head);
+        
+        return null;
+    }
+
+    @Override
+    public void deleteHead() {
+        if (head == null) {
+            return;
+        } else if (head == tail) {
             head = null;
             tail = null;
-            size = 0;
-            return;
+        } else {
+            head = head.getNext();
+            tail.setNext(head);
         }
-        SNode<T> curr = head;
-        while (curr.getNext() != tail) {
-            curr = curr.getNext();
-        }
-        curr.setNext(head);
-        tail = curr;
         size--;
     }
 
     @Override
-    public void delete(SNode<T> node) {
-        if (size == 0) {
+    public void deleteTail() {
+        if (tail == null) {
+            return;
+        } else if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            SNode curr = head;
+            while (curr.getNext() != tail) {
+                curr = curr.getNext();
+            }
+            curr.setNext(head);
+            tail = curr;
+        }
+        size--;
+    }
+
+    @Override
+    public void delete(SNode node) {
+        if (head == null) {  
+            System.out.println("The list is empty.");
+            System.out.println();
             return;
         }
-        if (head.equals(node)) {
+        
+        if (node.equals(head)) {  
             deleteHead();
             return;
         }
-        if (tail.equals(node)) {
-            deleteTail();
+        
+        SNode prev = head;
+        while (prev.getNext() != head && !prev.getNext().equals(node)) {
+            prev = prev.getNext();
+        }
+        
+        if (prev.getNext() == head) {  
             return;
         }
-        SNode<T> curr = head;
-        while (curr.getNext() != head) {
-            if (curr.getNext().equals(node)) {
-                curr.setNext(curr.getNext().getNext());
-                size--;
+        
+        if (prev.getNext().equals(tail)) {  
+            tail = prev;
+        }
+        
+        prev.setNext(prev.getNext().getNext());
+        size--;
+    }
+    
+    @Override
+    public void sort() {
+        if (!sorted) {
+            if (head == null || head == tail) {
+                sorted = true;
                 return;
             }
-            curr = curr.getNext();
+            
+            SNode curr = head;
+            do {
+                SNode next = curr.getNext();
+                while (next != head) {
+                    if (curr.getData() > next.getData()) {
+                        int temp = curr.getData();
+                        curr.setData(next.getData());
+                        next.setData(temp);
+                    }
+                    next = next.getNext();
+                }
+                curr = curr.getNext();
+            } while (curr != head);
+            
+            sorted = true;
         }
     }
+
+    @Override
+    public void clear() {
+        if (head == null) {
+            return;
+        }
+        tail.setNext(null); 
+        head = null;
+        tail = null;
+        size = 0;
+        sorted = true;
+    }
+        
+    @Override
+    public void print() {
+        if (head == null) {
+            System.out.println("The list is empty.");
+        } else {
+            System.out.println("List length: " + size);
+            System.out.println("Sorted Status: " + (sorted ? "sorted" : "not sorted"));
+            System.out.print("List content: ");
+            SNode curr = head;
+            do {
+                System.out.print(curr.getData() + " ");
+                curr = curr.getNext();
+            } while (curr != head);
+            System.out.println();
+        }
+    }
+
 }
--- src/main/java/mylib/datastructures/test/CSLLTest.java
+++ src/main/java/mylib/datastructures/test/CSLLTest.java
@@ -4,33 +4,222 @@ import src.main.java.mylib.datastructures.linear.CSLL;
 import src.main.java.mylib.datastructures.nodes.SNode;
 
 public class CSLLTest {
-    public static void main(String[] args) {
-        CSLL<Integer> csll = new CSLL<>();
+    public static void main (String[] args) {
+    
+    // Test CSLL Class methods 
+    System.out.println();
+    System.out.println();
+    System.out.println("***Testing CSLL Class...***");
+    System.out.println();
 
-        System.out.println("Inserting values 1, 2, and 3 at tail");
-        csll.insertTail(new SNode<>(1));
-        csll.insertTail(new SNode<>(2));
-        csll.insertTail(new SNode<>(3));
-        System.out.println("Expected output: 1 2 3");
-        System.out.println("Actual output: ");
-        csll.print();
 
-        System.out.println("Deleting head");
-        csll.deleteHead();
-        System.out.println("Expected output: 2 3");
-        System.out.println("Actual output: ");
-        csll.print();
+    // create a new CSLL object by using overloaded constructor CSLL(SNode head) which calls SLL(SNode node)
+    CSLL csll1 = new CSLL(new SNode(2));
 
-        System.out.println("Deleting tail");
-        csll.deleteTail();
-        System.out.println("Expected output: 2");
-        System.out.println("Actual output: ");
-        csll.print();
+    System.out.println("Overloaded constructor CSLL(SNode head) created a new CSLL object \"csll1\".");
+    System.out.println();
 
-        System.out.println("Deleting value 2");
-        csll.delete(new SNode<>(2));
-        System.out.println("Expected output: ");
-        System.out.println("Actual output: ");
-        csll.print();
+    // expected result
+    System.out.println("Expected Result: ");
+    System.out.println("Size of csll1: 1"); 
+    System.out.println("Value of head node of csll2: 2"); 
+    System.out.println("Value of tail node of csll2: 2"); 
+    System.out.println();
+
+    // actual result 
+    System.out.println("Actual Result: ");
+    System.out.println("Size of csll1: " + csll1.getSize()); 
+    System.out.println("Value of head node of csll2: " + csll1.getHead().getData());
+    System.out.println("Value of tail node of csll2: " + csll1.getTail().getData());
+    System.out.println();
+
+
+    // create a new CSLL object by using default constructor CSLL()
+    CSLL csll2 = new CSLL();
+    System.out.println();
+    System.out.println("Default constructor CSLL() successfully created a new CSLL object \"csll2\".");
+    System.out.println();
+
+    // expected result
+    System.out.println("Expected Result: ");
+    System.out.println("Size of csll1: 0"); 
+    System.out.println("Value of head node of csll2: null"); 
+    System.out.println("Value of tail node of csll2: null"); 
+    System.out.println();
+
+    // actual result
+    System.out.println("Actual Result: ");
+    System.out.println("Size of csll1: " + csll2.getSize()); 
+    System.out.println("Value of head node of csll2: " + csll2.getHead());
+    System.out.println("Value of tail node of csll2: " + csll2.getTail());
+    System.out.println();
+    System.out.println();
+
+    // test intereted and modified insertHead(SNode node) method from SLL class
+    csll2.insertHead(new SNode(5));
+    System.out.println("insertHead(SNode node) method successfully inserted 5 to the head of \"csll2\".");
+
+    // test inherited and modified insertHead(SNode node) method from SLL class
+    csll2.insertHead(new SNode(2));
+    System.out.println("insertHead(SNode node) method successfully inserted 2 to the head of \"csll2\".");
+
+    // test inherited and modified insertTail(SNode node) method from SLL class
+    csll2.insertTail(new SNode(8));
+    System.out.println("insertTail(SNode node) method successfully inserted 8 to the tail of \"csll2\".");
+
+    // test inherited and modified insert(SNode node, int position) method from SLL class
+    csll2.insert(new SNode(3), 2);
+    System.out.println("insertHead(SNode node) method successfully inserted 3 to position 2 of \"csll2\".");
+    System.out.println();
+    
+    // expected result
+    System.out.println("Expected Result: ");
+    System.out.println("List Length : 4");
+    System.out.println("Sorted Status : not sorted");
+    System.out.println("List Content: 2 5 3 8");
+    System.out.println();
+
+    // actual result displayed by test()
+    System.out.println("Actual Result: ");
+    csll2.print();
+    System.out.println();
+
+
+    // test inherited and modified sort() method from SLL class
+    csll2.sort();
+    System.out.println();
+    System.out.println("sort() method successfully sorted data in \"csll2\" in an ascending order.");
+    System.out.println();
+
+    // expected result
+    System.out.println("Expected Result: ");
+    System.out.println("List Length : 4");
+    System.out.println("Sorted Status : sorted");
+    System.out.println("List Content: 2 3 5 8");
+    System.out.println();
+
+    // actual result displayed by test()
+    System.out.println("Actual Result: ");
+    csll2.print();
+    System.out.println();
+
+
+    // test inherited and modified sortedInsert(SNode node) method from SLL class
+    SNode node2 = new SNode(7);
+    System.out.println();
+    System.out.println("SNode(int data) method successfully created a SNode object with data 7.");;
+    csll2.sortedInsert(node2);
+    System.out.println("sortedInsert(SNode node) method successfully inserted a new SNode with \"data\" 7 in a sorted order.");
+    System.out.println();
+
+    // expected result
+    System.out.println("Expected Result: ");
+    System.out.println("List Length : 5");
+    System.out.println("Sorted Status : sorted");
+    System.out.println("List Content: 2 3 5 7 8");
+    System.out.println();
+
+    // actual result displayed by test()
+    System.out.println("Actual Result: ");
+    csll2.print();
+    System.out.println();
+
+
+    // test inherited and modified search(SNode node) method from SLL class, successful case
+    System.out.println();
+    System.out.println("Testing search(SNode node) method by using previously created SLL object \"csll2\"...");
+    System.out.println();
+
+    SNode nodeToSearch1 = new SNode(5);
+    System.out.println("SNode(int data) method successfully created a SNode object with data 5.");;
+
+    // expected result
+    System.out.print("Expected Search Result : ");
+    System.out.println("Found node with value 5");
+  
+    // actual result
+    System.out.print("Actual Search Result : ");
+    SNode foundNode1 = csll2.search(nodeToSearch1);
+    if (foundNode1 != null) {
+        System.out.println("Found node with value " + foundNode1.getData());
+    } else {
+        System.out.println("Node not found");
+    }
+
+    // test inherited and modified search(SNode node) method from SLL class, not successful case
+    SNode nodeToSearch2 = new SNode(1);
+    System.out.println();
+    System.out.println("Node(int data) method successfully created a Node object with data 1.");;
+
+    // expected result
+    System.out.print("Expected Search Result : ");
+    System.out.println("Node not found");
+  
+    // actual result
+    System.out.print("Actual Search Result : ");
+    SNode foundNode2 = csll2.search(nodeToSearch2);
+    if (foundNode2 != null) {
+        System.out.println("Found node with value " + foundNode2.getData());
+    } else {
+        System.out.println("Node not found");
+    }
+    System.out.println();
+
+    // test inherited and modified deleteHead() method from SLL class
+    csll2.deleteHead();
+    System.out.println();
+    System.out.println("deleteHead() method successfully deleted the head of \"csll2\".");
+    System.out.println();
+
+    // expected result
+    System.out.println("Expected Result: ");
+    System.out.println("List Length : 4");
+    System.out.println("Sorted Status : sorted");
+    System.out.println("List Content: 3 5 7 8");
+    System.out.println();
+
+    // actual result displayed by print()
+    System.out.println("Actual Result: ");
+    csll2.print();
+    System.out.println();
+
+
+    // test inherited and modified deleteTail() method from SLL class
+    csll2.deleteTail();
+    System.out.println();
+    System.out.println("deleteTail() method successfully deleted the tail of \"csll2\".");
+    System.out.println();
+
+    // expected result
+    System.out.println("Expected Result: ");
+    System.out.println("List Length : 3");
+    System.out.println("Sorted Status : sorted");
+    System.out.println("List Content: 3 5 7");
+    System.out.println();
+
+    // actual result displayed by print()
+    System.out.println("Actual Result: ");
+    csll2.print();
+    System.out.println();
+
+
+    // test inherited and modified clear() method from SLL class
+    csll2.clear();
+    System.out.println();
+    System.out.println("clear() method successfully removed all nodes in the previously created SLL object \"csll2\".");
+    System.out.println();
+
+    // expected result
+    System.out.print("Expected Result: ");
+    System.out.println("The list is empty.");
+
+    // actual result displayed by print()
+    System.out.print("Actual Result: ");
+    csll2.print();
+
+    // end of  the test
+    System.out.println();
+    System.out.println("***CSLL Class Testing Completed!***");
+    
     }
 }
